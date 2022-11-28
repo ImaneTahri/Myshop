691bbb04cc5e6a409ab77f6887cb5369972a16b1
unknown <itahri@my.centennialcollege.ca>
2022-11-28 15:37:40 +0300

    first levell app

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        
+        <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Products</h3>
+                        <ul>
+                            <li><a href="#">Electronics</a></li>
+                            <li><a href="#">Clothes</a></li>
+                            <li><a href="#">Books</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Legacy</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Careers</h3>
+                        <ul>
+                            <li><a href="#">Job openings</a></li>
+                            <li><a href="#">Employee success</a></li>
+                            <li><a href="#">Benefits</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+                        
+                    </div>
+                </div> 
+                <div class="Copyright">
+
+                <li><a href="https://www.imanetahri.ca/">By mane TAHRI</a></li>
+
+
+                </div>
+                </div> 
+        
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
